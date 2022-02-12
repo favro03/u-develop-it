1622ff1f63589df66afe69cc8da039528b2b01d9
tfavro <wetr9902@gmail.com>
2022-02-12 15:22:47 -0600

    added routes to call db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,8 +7,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'fullstack#1',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
-
+//returns all the data in the db
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (NOT FOUND)  *needs to be at the end of routes)
 app.use((req, res) => {
